1eec6d6e4abaacdce58a9d387df1748f382c685b
hanryyyy <tuonghantruong@gmail.com>
2025-03-23 16:18:01 +0700

    first commit fix with code

---
+++ shopping/about-us.php
@@ -0,0 +1,53 @@
+<?php session_start();
+include_once('includes/config.php');
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shopping Portal | Category wise Shop </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+       <!--  <link href="css/bootstrap.min.css" rel="stylesheet" /> -->
+    </head>
+    <body>
+<?php include_once('includes/header.php');?>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">About Us</h1>
+        
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                At my Mintmade, we are passionate about bringing the latest fashion trends to Vietnam. Our collections are inspired by global styles while embracing the unique essence of Vietnamese fashion. From chic streetwear to elegant evening outfits, we ensure that every piece is designed with high-quality materials and modern aesthetics.
+        <br />We believe that fashion is more than just clothing—it’s a way to express personality and confidence. Whether you’re looking for everyday essentials or standout pieces for special occasions, we’ve got you covered. Stay ahead of the trends with [Your Brand Name] and redefine your style with the latest fashion in Vietnam.
+
+                </div>
+            </div>
+
+ 
+</div>
+        </section>
+        <!-- Footer-->
+   <?php include_once('includes/footer.php'); ?>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
